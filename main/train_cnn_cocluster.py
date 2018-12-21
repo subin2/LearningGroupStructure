7763462a09cc96cb663253e81f334a2e0c8479b3
mrjiao <mrjiao2018@163.com>
2018-12-21 11:34:08 +0800

    cnn_cocluster综合实现, step2: finish cnn_cocluster_config.py

--- main/train_cnn_cocluster.py
+++ main/train_cnn_cocluster.py
@@ -5,11 +5,12 @@
 # @IDE     : PyCharm Community Edition
 """
     将em中的模型转化成co-cluster的综合版本, 并进行train测试
-    todo: 0. 完善data_processor.py                    finished
-    todo: 1. 建立一个前向传播模型 cnn_cocluster
-    todo: 2. 使用co-cluster对W进行优化 cnn_cocluster
-    todo: 3. 更新网络结构 cnn_cocluster
-    todo: 4. 打印结果 ./tools/show_result.py
+    todo: 1. 完善data_processor.py                     finished
+    todo: 2. 完善cnn_cocluster_config.py, 设置全局参数   finished
+    todo: 3. 建立一个前向传播模型 cnn_cocluster
+    todo: 4. 使用co-cluster对W进行优化 cnn_cocluster
+    todo: 5. 更新网络结构 cnn_cocluster
+    todo: 6. 打印结果 ./tools/show_result.py
 """
 from data_provider import data_processor
 
